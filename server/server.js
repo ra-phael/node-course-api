f0841a7bd12cda8b31ab22d7208f1fc84b8d0fe6
Raphael <rapssail@hotmail.fr>
2019-01-10 17:02:52 +0100

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser'); //takes JSON and convert into an object
 const { ObjectID } = require('mongodb');
@@ -50,7 +51,6 @@ app.get('/todos/:id', (req, res) => {
 
 })
 
-
 app.delete('/todos/:id', (req, res) => {
     const id = req.params.id;
 
@@ -69,6 +69,31 @@ app.delete('/todos/:id', (req, res) => {
 
 });
 
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send('Invalid id');
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send()
+    })
+})
 
 app.listen(port, () => {
     console.log('Started on port 3000');
