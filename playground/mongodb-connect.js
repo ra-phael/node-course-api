df6d62d782203fbe33243afbad6d6e98d4c23497
Raphael <rapssail@hotmail.fr>
2019-01-10 09:16:53 +0100

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,28 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// let obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Sth to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // })
+
+    client.close();
+});
+
---
+++ playground/mongodb-find.js
@@ -0,0 +1,28 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5c3607d8fbb5341314fce1cb')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2))
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err)
+    // })
+
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch todos', err)
+    })
+
+    client.close();
+});
+
