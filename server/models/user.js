fe3f3d6e06af93af55b9ccdba7d17ce028d3be95
Raphael <rapssail@hotmail.fr>
2019-01-11 17:56:21 +0100

    add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,16 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    let user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+};
+
 UserSchema.statics.findByToken = function (token) {
     const User = this;
     let decoded;
--- server/server.js
+++ server/server.js
@@ -130,6 +130,16 @@ app.post('/users/login', (req, res) => {
     }) 
 })
 
+// LOG OUT
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    })    
+});
+
 
 app.listen(port, () => {
     console.log('Started on port 3000');
