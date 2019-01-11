2938fadd9565f702f599a677e08d1f3f50b8572d
Raphael <rapssail@hotmail.fr>
2019-01-11 18:21:34 +0100

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -298,7 +298,7 @@ describe('POST /users/login', () => {
             .end((err, res) => {
                 if (err) {
                     return done(err);
-                }
+                };
 
                 User.findById(users[1]._id).then((user) => {
                     expect(user.tokens.length).toBe(0);
@@ -309,5 +309,25 @@ describe('POST /users/login', () => {
 
 })
 
+
+describe('DELETE /users/me/token', () => {
+   it('should remove auth token on logout', (done) => {
+        request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            };
+
+            User.findById(users[0]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((e) => done(e));
+
+        })
+   }) 
+});
 
 
